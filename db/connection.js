1605b8c81538a2a3b381dfa20eca7f9120c771c1
Hammad Malik <hmd.node@gmail.com>
2018-01-29 16:39:40 -0500

    Update db/connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/project-3')
+// mongoose.connect('mongodb://localhost/project-3')
+
+const mongoUri = 'mongodb://localhost/project-3'
+
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(process.env.MLAB_URL, { useMongoClient: true })
+} else {
+  mongoose.connect(mongoUri, { useMongoClient: true })
+    .then(connection => console.log(`connection established to db ${connection.db.databaseName}`))
+    .catch(error => console.log('Connection Failed!', error))
+}
 
 mongoose.Promise = Promise
 module.exports = mongoose
