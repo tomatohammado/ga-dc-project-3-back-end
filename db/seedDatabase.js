21ee1ab780e706edede3f7c2c828970156a512be
Hammad Malik <hmd.node@gmail.com>
2018-01-31 11:45:55 -0500

    Rename and comment new single db/seedDatabase.js file

--- db/seedDatabase.js
+++ db/seedDatabase.js
@@ -1,17 +1,18 @@
 const Provider = require('./models/provider')
 const Movie = require('./models/movie')
-const seedDataProvider = require('./providerSeedData.json')
-const seedDataMovie = require('./movieSeedData.json')
+const providersSeed = require('./seedDataProviders.json')
+const moviesSeed = require('./seedDataMovies.json')
 
+// 1. Empty the `providers` collection, and reseed it
 Provider.remove({})
   .then(_ => {
-    Provider.create(seedDataProvider)
+    Provider.create(providersSeed)
     .then(_ => {
+      // 2. then, query the new `providers` collection, and use the generated `_id` fields to replace the provider name strings in the `movies'` seed file
       Provider.find({})
         .then(providers => {
           let providerData = providers.map(obj => Object.assign({}, obj._doc))
-          // let newSeedDataMovie = seedDataMovie.map(obj => Object.assign({}, obj))
-          let newSeedDataMovie = seedDataMovie.map(movie => {
+          let newMoviesSeed = moviesSeed.map(movie => {
             movie.providers = movie.providers.map(providerString => {
               let providerIndex = providerData.findIndex(providerObj => {
                 return providerObj.name === providerString
@@ -21,9 +22,10 @@ Provider.remove({})
             return movie
           })
 
+          // 3. Seed the `movies` collection similar to how we seeded the `providers`, but with the new, transformed seed file
           Movie.remove({})
             .then(_ => {
-              Movie.create(newSeedDataMovie)
+              Movie.create(newMoviesSeed)
                 .then(_ => {
                   process.exit()
                 })
