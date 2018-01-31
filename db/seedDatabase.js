17ad88142722408dba99c2b4647f9d504c8e8ad6
Hammad Malik <hmd.node@gmail.com>
2018-01-31 14:17:08 -0500

    Refactor db/seedDatabase.js to implement utility.js

--- db/seedDatabase.js
+++ db/seedDatabase.js
@@ -3,6 +3,9 @@ const Movie = require('./models/movie')
 const providersSeed = require('./seedDataProviders.json')
 const moviesSeed = require('./seedDataMovies.json')
 
+const utility = require('../utility')
+const transformProvidersArray = utility.transformProvidersArray
+
 // 1. Empty the `providers` collection, and reseed it
 Provider.remove({})
   .then(_ => {
@@ -11,14 +14,8 @@ Provider.remove({})
       // 2. then, query the new `providers` collection, and use the generated `_id` fields to replace the provider name strings in the `movies'` seed file
       Provider.find({})
         .then(providers => {
-          let providerData = providers.map(obj => Object.assign({}, obj._doc))
           let newMoviesSeed = moviesSeed.map(movie => {
-            movie.providers = movie.providers.map(providerString => {
-              let providerIndex = providerData.findIndex(providerObj => {
-                return providerObj.name === providerString
-              })
-              return providerData[providerIndex]._id
-            })
+            movie.providers = transformProvidersArray(movie.providers, providers)
             return movie
           })
 
