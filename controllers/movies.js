956e03d1b9da677e312f12e665acddcd7356b513
Hammad Malik <hmd.node@gmail.com>
2018-01-31 11:49:59 -0500

    Refactor getMovies controller method
    Looking good!

--- controllers/movies.js
+++ controllers/movies.js
@@ -3,54 +3,18 @@ const Movie = require('../db/models/movie')
 
 function getMovies (req, res) {
   Movie.find({})
-    .then(movies => {
+    .populate('providers')
+    .exec((err, movies) => {
+      if (err) console.log(err)
       res.json(movies)
     })
 }
-// the create functionality
-// function postMovie (req, res) {
-//   let newMovie = Object.assign({}, req.body)
-//   let rawProviders = newMovie.providers
-//   let cleanedProviders = rawProviders.split(',').map(provider => provider.trim())
-//   let providersData
-//   let providerIndex
-//   let transformedProviders
-//   Provider.find({})
-//     .then(providers => {
-//       providersData = providers.slice()
-//       transformedProviders = cleanedProviders.map(provider => {
-//         providerIndex = providersData.findIndex(element => {
-//           return element.name === provider
-//         })
-//         if (providerIndex === -1) return ''
-//         else {
-//           ++providersData[providerIndex].totalMovies
-//           return providersData[providerIndex]._id
-//         }
-//       })
-//       newMovie.providers = transformedProviders.slice()
-//       console.log(newMovie.providers)
-
-//       Provider.update({
-//         '_id': {$in: newMovie.providers}},
-//         {$inc: {totalMovies: 1}},
-//         {multi: true})
-//         .then(_ => {
-//           Movie.create(newMovie)
-//             .then(_ => {
-//               Movie.find({})
-//                 .then(movies => res.json(movies))
-//             })
-//         })
-//     })
-// }
 
 function postMovie (req, res) {
   Movie.create(req.body)
     .then(movie => res.json(movie))
 }
 
-// The update functionality
 function putMovie (req, res) {
   Movie.findByIdAndUpdate(req.params.id, req.body, { new: true })
     .then(movie => {
