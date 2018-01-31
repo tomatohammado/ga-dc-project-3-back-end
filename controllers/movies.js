d6608e965a1dda47a826098f7a08c3d36832b356
Hammad Malik <hmd.node@gmail.com>
2018-01-31 12:34:31 -0500

    Fix postMovie controller method

--- controllers/movies.js
+++ controllers/movies.js
@@ -22,14 +22,12 @@ function postMovie (req, res) {
   Provider.find({})
     .then(providers => {
       let providerData = providers.map(obj => Object.assign({}, obj._doc))
-      let newMovie = req.body.map(movie => {
-        movie.providers = movie.providers.map(providerString => {
-          let providerIndex = providerData.findIndex(providerObj => {
-            return providerObj.name === providerString
-          })
-          return providerData[providerIndex]._id
+      let newMovie = Object.assign({}, req.body.data)
+      newMovie.providers = newMovie.providers.map(providerString => {
+        let providerIndex = providerData.findIndex(providerObj => {
+          return providerObj.name === providerString
         })
-        return movie
+        return providerData[providerIndex]._id
       })
       Movie.create(newMovie)
         .then(movie => res.json(movie))
